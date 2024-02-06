b37401edc4ac95cc48d634b8db2989ab00c98b91
Starlong <starlong.lu@gmail.com>
2024-02-06 23:49:37 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 const antfu = require('@antfu/eslint-config').default
-const unocss = require('@unocss/eslint-plugin')
 
 module.exports = antfu(
   {
+    unocss: true,
+    formatters: true,
     rules: {
       // console 规则
       'no-console': [1, { allow: ['info', 'warn', 'error'] }],
@@ -10,24 +11,10 @@ module.exports = antfu(
       'antfu/if-newline': 'off',
       // 强制执行 1tbs 风格的大括号样式
       'style/brace-style': ['error', '1tbs'],
-      // 允许（但不要求）使用尾随逗号
-      'style/comma-dangle': ['error', 'only-multiline'],
-      // 代码最大长度限制
-      'style/max-len': ['warn', {
-        code: 120,
-        ignorePattern: '^\\s*class=',
-        ignoreComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true
-      }],
-
       // 解决 uv-switch 中的 active-color 属性样式不生效的问题
       'vue/attribute-hyphenation': ['error', 'always', {
-        ignore: ['activeColor']
-      }]
-    }
+        ignore: ['activeColor'],
+      }],
+    },
   },
-  unocss.configs.flat,
 )
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -4,3 +4,9 @@ declare module 'vue' {
   type Hooks = App.AppInstance & Page.PageInstance
   interface ComponentCustomOptions extends Hooks {}
 }
+
+declare global {
+  interface Uni {
+    $uv: any
+  }
+}
