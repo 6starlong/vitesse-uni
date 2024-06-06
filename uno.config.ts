7e381095fb2ffa5bb5312795b5963748fdac53d6
Starlong <starlong.lu@gmail.com>
2024-06-06 13:41:16 +0800

    chore: update deps

--- uno.config.ts
+++ uno.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     [/^(flex|grid)-y-center/g, () => 'items-center'],
 
     // 按钮
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ['btn', 'inline-block cursor-pointer rounded bg-teal-700 px-4 py-1 text-white lh-none disabled:cursor-default disabled:bg-gray-600 hover:bg-teal-800 disabled:opacity-50 !outline-none'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
 
     // 底部安全区域
--- vite.config.ts
+++ vite.config.ts
@@ -8,59 +8,67 @@ import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
-import Unocss from 'unocss/vite'
+// import Unocss from 'unocss/vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${resolve(__dirname, 'src')}/`,
+export default defineConfig(async () => {
+  /**
+   * unocss@0.59.x版本后仅支持ESM，使用动态导入来兼容uni(Uni采用的是CommonJS，不支持ESM库)
+   * @see https://github.com/dcloudio/uni-app/issues/4815
+   */
+  const UnoCSS = await import('unocss/vite').then(i => i.default)
+
+  return {
+    resolve: {
+      alias: {
+        '~/': `${resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-  plugins: [
+    plugins: [
     // https://github.com/uni-helper/vite-plugin-uni-pages
-    UniPages({
-      dts: 'src/uni-pages.d.ts',
-    }),
+      UniPages({
+        dts: 'src/uni-pages.d.ts',
+      }),
 
-    // https://github.com/uni-helper/vite-plugin-uni-layouts
-    UniLayouts(),
+      // https://github.com/uni-helper/vite-plugin-uni-layouts
+      UniLayouts(),
 
-    // https://github.com/unplugin/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        'uni-app',
-      ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: [
-        './src/composables',
-        './src/stores',
-      ],
-      vueTemplate: true,
-    }),
+      // https://github.com/unplugin/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          'uni-app',
+        ],
+        dts: 'src/auto-imports.d.ts',
+        dirs: [
+          './src/composables',
+          './src/stores',
+        ],
+        vueTemplate: true,
+      }),
 
-    // https://github.com/unplugin/unplugin-vue-components
-    Components({
-      dts: 'src/components.d.ts',
-      types: []
-    }),
+      // https://github.com/unplugin/unplugin-vue-components
+      Components({
+        dts: 'src/components.d.ts',
+        types: [],
+      }),
 
-    // https://vue-macros.sxzz.moe/zh-CN/features/reactivity-transform.html
-    ReactivityTransform(),
+      // https://vue-macros.sxzz.moe/zh-CN/features/reactivity-transform.html
+      ReactivityTransform(),
 
-    // https://github.com/dcloudio/uni-app
-    uni(),
+      // https://github.com/dcloudio/uni-app
+      uni(),
 
-    // https://github.com/unocss/unocss
-    Unocss(),
+      // https://github.com/unocss/unocss
+      UnoCSS(),
 
-    // https://github.com/webfansplz/vite-plugin-vue-devtools
-    VueDevTools(),
-  ],
-  // https://github.com/vitest-dev/vitest
-  test: {
-    environment: 'jsdom'
+      // https://github.com/webfansplz/vite-plugin-vue-devtools
+      VueDevTools(),
+    ],
+    // https://github.com/vitest-dev/vitest
+    test: {
+      environment: 'jsdom',
+    },
   }
 })
