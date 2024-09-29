353a70441d73d23e2024326f2525622fb5294acf
Starlong <starlong.lu@gmail.com>
2024-09-29 14:55:56 +0800

    chore: update deps, types

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -4,9 +4,3 @@ declare module 'vue' {
   type Hooks = App.AppInstance & Page.PageInstance
   interface ComponentCustomOptions extends Hooks {}
 }
-
-declare global {
-  interface Uni {
-    $uv: any
-  }
-}
